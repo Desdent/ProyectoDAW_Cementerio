639244008a1bff4f717d7f9bcaff2740c8605b49
Desdent <oleogm@gmail.com>
2026-02-03 21:28:19 +0100

    añadida funcion de mapa interactivo

--- FrontendCemetery/src/app/features/search-component/search-component.ts
+++ FrontendCemetery/src/app/features/search-component/search-component.ts
@@ -1,8 +1,13 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CementerioService } from '../../core/services/cementerioService';
 import { Cementerio } from '../../interfaces/cementerio/cementerio';
 import { RouterLink } from '@angular/router';
+import * as bootstrap from 'bootstrap';
+import { ZonaService } from '../../core/services/zonaService';
+import { Zona } from '../../interfaces/zona/zona';
+import { Router } from '@angular/router';
+// import imageMapResize from 'image-map-resizer';
 
 @Component({
   selector: 'app-search-component',
@@ -13,9 +18,18 @@ import { RouterLink } from '@angular/router';
 })
 export class SearchComponent {
   private cementerioService = inject(CementerioService);
+  private zonaService = inject(ZonaService);
+  private router = inject(Router);
 
+  @ViewChild('htmlModal') htmlModal!: ElementRef;
+
+  modalBootstrap: bootstrap.Modal | undefined;
   cementerios = signal<Cementerio[]>([]);
   filtroTexto = signal<string>('');
+  mapaSeleccionado = signal<string>('');
+  coordenadas = signal<string[]>([]);
+  zonasCementerio = signal<Zona[]>([]);
+  cementerioIdSeleccionado = signal<number>(0);
 
   /**
    * Calculo la lista de cementerios filtrados basándome en el texto de búsqueda introducido.
@@ -56,5 +70,44 @@ export class SearchComponent {
    */
   verMapa(cementerio: Cementerio) {
     console.log('Abriendo detalles de:', cementerio.nombre);
+  }
+
+  abrirModal(id: number) {
+    this.cementerioIdSeleccionado.set(id);
+    this.cementerioService.find(id).subscribe({
+      next: (data) => {
+        this.mapaSeleccionado.set(data.mapa!);
+      },
+    });
+    this.zonaService.findAllByCementerioId(id).subscribe({
+      next: (data) => {
+        this.zonasCementerio.set(data);
+      },
+    });
+    if (this.htmlModal && this.htmlModal.nativeElement) {
+      this.modalBootstrap = new bootstrap.Modal(this.htmlModal.nativeElement);
+      this.modalBootstrap.show();
+    }
+  }
+
+  cerrarModal() {
+    if (this.modalBootstrap) {
+      this.modalBootstrap.hide();
+    }
+  }
+
+  viewBoxValue = signal<string>('0 0 1000 1000'); // Valor por defecto
+
+  // Método para detectar el tamaño real de la imagen al cargar
+  onImageLoad(event: any) {
+    const img = event.target;
+    // Ajustamos el sistema de coordenadas del SVG al tamaño real de la imagen
+    this.viewBoxValue.set(`0 0 ${img.naturalWidth} ${img.naturalHeight}`);
+  }
+
+  navegarAZona(zonaId: number) {
+    this.cerrarModal(); // Cerramos el modal antes de irnos
+    // Navegamos usando el router de Angular
+    this.router.navigate(['/cementerio', this.cementerioIdSeleccionado(), { zona: zonaId }]);
   }
 }
